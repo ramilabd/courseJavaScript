d43b75341592a72def7f2d5fd464b60ca325d644
ram-smart <ramsmart@mail.ru>
2018-12-02 00:08:08 +0500

    добавил функцию assert для тестирования других функций

--- myModules/myModules.js
+++ myModules/myModules.js
@@ -1,2 +1,17 @@
 export const length = str => str.length;
 export const toUpperCase = str => str.toUpperCase();
+
+/**
+ * Функция assert - функция для тестирования кода
+ * @param  {[type]} should    [то, что должна вернуть функция предаваемая третьим параметром - func]
+ * @param  {[type]} paramFunc [параметры для функции func]
+ * @param  {[type]} func      [func принимает paramFunc и возвращает значение с которым сравнивается should]
+ * @return {[type]}           [возвращает true если требуеое значение (should) равно возвращеному функцией func]
+ */
+export const assert = (should, paramFunc, func) => {
+	if (should === func(paramFunc)) {
+		console.log(true);
+	} else {
+		console.log(false);
+	}
+};
