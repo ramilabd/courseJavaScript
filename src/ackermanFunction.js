1d8356eaec910a5984a65fbf49d591257f09de4a
ram-smart <ramsmart@mail.ru>
2018-12-06 01:21:24 +0500

    добавил вызов тест-функции assert

--- src/ackermanFunction.js
+++ src/ackermanFunction.js
@@ -1,5 +1,5 @@
 
-import {assert} from './myModules/myModules';
+import {assert} from './myModules';
 
 const akkerman = (m, n) => {
   if (m === 0) return n + 1;
@@ -11,6 +11,13 @@ const akkerman = (m, n) => {
   }
 };
 
-assert(1, 0, akkerman);
-
-// console.log(akkerman(5, 0));
+assert(1, 0, 0, akkerman);
+assert(2, 1, 0, akkerman);
+assert(3, 2, 0, akkerman);
+assert(5, 3, 0, akkerman);
+assert(13, 4, 0, akkerman);
+assert(2, 0, 1, akkerman);
+assert(3, 0, 2, akkerman);
+assert(4, 0, 3, akkerman);
+assert(5, 0, 4, akkerman);
+assert(6, 0, 5, akkerman);
