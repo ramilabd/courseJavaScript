166551634e81a1973dd5ee039be1f6272bb136b9
ram-smart <ramsmart@mail.ru>
2018-12-06 01:32:21 +0500

    добавил решение хекслета

--- src/ackermanFunction.js
+++ src/ackermanFunction.js
@@ -1,23 +1,37 @@
+// Реализуйте и экспортируйте по умолчанию функцию Аккермана.
 
 import {assert} from './myModules';
 
-const akkerman = (m, n) => {
+// Мое решение
+const ackerman1 = (m, n) => {
   if (m === 0) return n + 1;
   if (m > 0 && n === 0){
-    return akkerman(m - 1, 1);
+    return ackerman1(m - 1, 1);
   }
   if (m > 0 && n > 0){
-    return akkerman(m - 1, akkerman(m, n -1))
+    return ackerman1(m - 1, ackerman1(m, n -1))
   }
 };
 
-assert(1, 0, 0, akkerman);
-assert(2, 1, 0, akkerman);
-assert(3, 2, 0, akkerman);
-assert(5, 3, 0, akkerman);
-assert(13, 4, 0, akkerman);
-assert(2, 0, 1, akkerman);
-assert(3, 0, 2, akkerman);
-assert(4, 0, 3, akkerman);
-assert(5, 0, 4, akkerman);
-assert(6, 0, 5, akkerman);
+// Решение Хекслета
+const ackerman2 = (m, n) => {
+  if (m === 0) {
+    return n + 1;
+  } else if (n === 0 && m > 0) {
+    return ackerman2(m - 1, 1);
+  }
+
+  return ackerman2(m - 1, ackerman2(m, n - 1));
+};
+
+// Тестирование
+assert(1, 0, 0, ackerman1);
+assert(2, 1, 0, ackerman1);
+assert(3, 2, 0, ackerman1);
+assert(5, 3, 0, ackerman1);
+assert(13, 4, 0, ackerman1);
+assert(2, 0, 1, ackerman1);
+assert(3, 0, 2, ackerman1);
+assert(4, 0, 3, ackerman1);
+assert(5, 0, 4, ackerman1);
+assert(6, 0, 5, ackerman1);
