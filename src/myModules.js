297ec4bcfc2ceba4d311f3606aad70958b5df580
ram-smart <ramsmart@mail.ru>
2018-12-06 01:22:29 +0500

    в функции assert добавил параметр

--- src/myModules.js
+++ src/myModules.js
@@ -4,12 +4,13 @@ export const toUpperCase = str => str.toUpperCase();
 /**
  * Функция assert - функция для тестирования кода
  * @param  {[type]} should    [то, что должна вернуть функция предаваемая третьим параметром - func]
- * @param  {[type]} paramFunc [параметры для функции func]
+ * @param  {[type]} paramFunc1 [параметры для функции func]
+ * @param  {[type]} paramFunc2 [параметры для функции func]
  * @param  {[type]} func      [func принимает paramFunc и возвращает значение с которым сравнивается should]
  * @return {[type]}           [возвращает true если требуеое значение (should) равно возвращеному функцией func]
  */
-export const assert = (should, paramFunc, func) => {
-	if (should === func(paramFunc)) {
+export const assert = (should, paramFunc1, paramFunc2, func) => {
+	if (should === func(paramFunc1, paramFunc2)) {
 		console.log(true);
 	} else {
 		console.log(false);
