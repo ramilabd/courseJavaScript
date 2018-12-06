8afab6b0865f651625011d4ef657612b5d5c850e
ram-smart <ramsmart@mail.ru>
2018-12-07 00:17:39 +0500

    выполнил решение "Преобразование DNA в RNA"

--- src/dnaToRna.js
+++ src/dnaToRna.js
@@ -4,19 +4,61 @@
 * Если в переданной цепи ДНК встретится "незнакомый" нуклеотид (не являющийся одним из четырех перечисленных выше),
 * то функция должна вернуть null.
 */
-import {assert} from './src/myModules';
+import {assertOneParam} from './myModules';
+
 // Мое решение
-
-
-
-
+const dnaToRna = (dna) => {
+  if (dna.length === 0) return '';
+  let rna = '';
+  for (let i = 0; i < dna.length; i += 1){
+    switch(dna[i]) {
+      case 'G':
+        rna += 'C';
+        break;
+      case 'C':
+        rna += 'G';
+        break;
+      case 'T':
+        rna += 'A';
+        break;
+      case 'A':
+        rna += 'U';
+        break;
+      default:
+        rna = null;
+        return rna;
+    }
+  }
+  return rna;
+};
 
 // Решение Хекслета
+export default (n) => {
+  let rna = '';
+  for (let i = 0; i < n.length; i += 1) {
+    switch (n[i]) {
+      case 'G':
+        rna += 'C';
+        break;
+      case 'C':
+        rna += 'G';
+        break;
+      case 'T':
+        rna += 'A';
+        break;
+      case 'A':
+        rna += 'U';
+        break;
+      default:
+        return null;
+    }
+  }
 
+  return rna;
+};
 
 // Тестирование
-
-dnaToRna('ACGTGGTCTTAA'); // 'UGCACCAGAAUU'
-dnaToRna('CCGTA'); // 'GGCAU'
-dnaToRna(''); // ''
-dnaToRna('ACNTG'); // null
+assertOneParam('UGCACCAGAAUU', 'ACGTGGTCTTAA', dnaToRna);
+assertOneParam('GGCAU', 'CCGTA', dnaToRna);
+assertOneParam('', '', dnaToRna);
+assertOneParam(null, 'ACNTG', dnaToRna);
