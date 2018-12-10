96b57598f630eb72c3aabfde91e46589f797cb1b
ram-smart <ramsmart@mail.ru>
2018-12-10 15:24:57 +0500

    добавил выисление факториала с применением итеративного процесса

--- src/factorial.js
+++ src/factorial.js
@@ -1,8 +1,21 @@
-const factorial = number => {
+const factorialRecursion = number => {
 	if (number === 0 || number === 1) {
 		return 1;
 	}
 	return number * factorial(number - 1);
 };
 
-console.log(factorial(5));
+// Вычисление факториала с прменением итеративного процесса.
+const factorialIterations = (n) => {
+  const iter = (counter, acc) => {
+    if (counter > n) {
+      return acc;
+    }
+    return iter(counter + 1, counter * acc);
+  };
+
+  return iter(1, 1);
+};
+
+console.log(factorialRecursion(5));
+console.log(factorialIterations(5));
