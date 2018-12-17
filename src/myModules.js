a233b142c35602cfd44769bc72ddaf567a2629bb
ram-smart <ramsmart@mail.ru>
2018-12-17 21:57:58 +0500

    added implementation of pairs

--- src/myModules.js
+++ src/myModules.js
@@ -23,3 +23,17 @@ export const assertTwoParam = (should, paramFunc1, paramFunc2, func) => {
     console.log(false);
   }
 };
+
+// Implementation of pairs.
+export const cons = (a, b) => message => {
+  switch (message) {
+    case 'car':
+      return a;
+    case 'cdr':
+      return b;
+  }
+};
+
+export const car = pair => pair('car');
+export const cdr = pair => pair('cdr');
+export const toString = pair => `${car(pair)},${cdr(pair)}`;
