d7d2d86af9f39dffce5feee261d6a854119efa5f
ram-smart <ramsmart@mail.ru>
2020-10-18 09:47:47 +0500

    added new function "buildDefinitionList"

--- src/arrays/arrays.js
+++ src/arrays/arrays.js
@@ -25,3 +25,22 @@ export const reverseInPlace = (items) => {
 
   return items;
 };
+
+export const buildDefinitionList = (definitions) => {
+  if (definitions.length === 0) {
+    return '';
+  }
+
+  const parts = [];
+
+  for (const definition of definitions) {
+    const name = definition[0];
+    const description = definition[1];
+    parts.push(`<dt>${name}</dt><dd>${description}</dd>`);
+  }
+
+  const innerValue = parts.join('');
+  const result = `<dl>${innerValue}</dl>`;
+
+  return result;
+};
